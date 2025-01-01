7e13cff03453ab7d6ed3e0fe793842767380f1a3
Scar Face <peteradaaku@gmail.com>
2025-01-01 20:45:14 +0100

    stuffs

---
+++ buildinTypes.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Player struct {
+	name        string
+	health      int
+	attackPower float64
+}
+
+func (player Player) getHealth() int {
+	return player.health
+}
+
+func main() {
+	// player := Player{
+	// 	name:        "scar face",
+	// 	health:      101,
+	// 	attackPower: 45.7,
+	// }
+
+	users := make(map[string]int)
+
+	users["scar"] = 3
+	users["face"] = 2
+
+	// age, ok := users["scar"]
+
+	// if !ok {
+	// 	fmt.Println("scar does not exist in the map")
+	// } else {
+	// 	fmt.Println("scar exist in the map", age)
+	// }
+
+	//fmt.Printf("health: %d\n", player.getHealth())
+	//fmt.Printf("age, %+d\n", age)
+
+	for k, v := range users {
+		fmt.Printf("the key %s and the value %d\n", k, v)
+	}
+}
