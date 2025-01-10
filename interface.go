bb7c30d758b132e067f15e95b726ee3b3353e705
Scar Face <peteradaaku@gmail.com>
2025-01-10 09:16:52 +0100

    inerfaces

--- interface.go
+++ interface.go
@@ -1,49 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type Shape interface {
-	Area() float64
-	Perimeter() float64
+type NumberStorer interface {
+	GetAll() ([]int, error)
+	Put(int) error
 }
 
-type Circle struct {
-	radius float64
+type MongoDBNumberStore struct {
 }
 
-type Rectangle struct {
-	length, width float64
+func (m MongoDBNumberStore) GetAll() ([]int, error) {
+	return []int{1, 2, 3, 4}, nil
 }
 
-func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
+func (m MongoDBNumberStore) Put(number int) error {
+	fmt.Println("store the number into the data base")
+	return nil
 }
 
-func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func (r Rectangle) Area() float64 {
-	return r.length * r.width
-}
-
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.length + r.width)
+type ApiSever struct {
+	numberStore NumberStorer
 }
 
 func main() {
+	apiServer := ApiSever{
+		numberStore: MongoDBNumberStore{},
+	}
 
-	var s Shape
+	numbers, err := apiServer.numberStore.GetAll()
+	if err != nil {
+		panic(err)
+	}
 
-	s = Circle{radius: 20}
-	fmt.Println("Area:", s.Area())
-	fmt.Println("Perimeter:", s.Perimeter())
-
-	s = Rectangle{length: 4, width: 5}
-	fmt.Println("R Area:", s.Area())
-	fmt.Println("R Area:", s.Perimeter())
-
+	fmt.Println(numbers)
 }
---
+++ interface1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+type Rectangle struct {
+	length, width float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.length * r.width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.length + r.width)
+}
+
+func main5() {
+
+	var s Shape
+
+	s = Circle{radius: 20}
+	fmt.Println("Area:", s.Area())
+	fmt.Println("Perimeter:", s.Perimeter())
+
+	s = Rectangle{length: 4, width: 5}
+	fmt.Println("R Area:", s.Area())
+	fmt.Println("R Area:", s.Perimeter())
+
+}
